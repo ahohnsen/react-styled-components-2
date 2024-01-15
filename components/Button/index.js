0cec5aa4f0f76aac500f2783da4894c437f377b8
Andrea Hohnsen <a.hohnsen@mail.de>
2024-01-15 14:51:40 +0100

    Move Button to components folder

---
+++ components/Button/index.js
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+  border: none;
+  cursor: pointer;
+  padding: 1rem;
+  color: white;
+  border-radius: 4px;
+  background-color: ${(props) =>
+    props.$color === "danger" ? "var(--color-danger)" : "var(--color-primary)"};
+
+  /* background-color: ${({ $color }) =>
+    $color === "danger" ? "var(--color-danger)" : "var(--color-primary)"}; */
+
+  ${(props) =>
+    props.$variant === "text" &&
+    css`
+      background-color: transparent;
+      color: var(--color-primary);
+    `}
+
+  ${(props) =>
+    props.$variant === "outlined" &&
+    css`
+      background-color: transparent;
+      color: var(--color-primary);
+      border: solid 2px var(--color-primary);
+    `};
+
+  ${(props) =>
+    props.$variant === "contained" &&
+    css`
+      background-color: var(--color-primary);
+      color: white;
+    `};
+
+  &:hover {
+    color: var(--color-primary);
+    background-color: var(--color-secondary);
+  }
+`;
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,12 @@
-import styled, { css } from "styled-components";
-
-const Button = styled.button`
-  border: none;
-  cursor: pointer;
-  padding: 1rem;
-  color: white;
-  border-radius: 4px;
-  background-color: ${(props) =>
-    props.$color === "danger" ? "var(--color-danger)" : "var(--color-primary)"};
-
-  /* background-color: ${({ $color }) =>
-    $color === "danger" ? "var(--color-danger)" : "var(--color-primary)"}; */
-
-  ${(props) =>
-    props.$variant === "text" &&
-    css`
-      background-color: transparent;
-      color: var(--color-primary);
-    `}
-
-  ${(props) =>
-    props.$variant === "outlined" &&
-    css`
-      background-color: transparent;
-      color: var(--color-primary);
-      border: solid 2px var(--color-primary);
-    `};
-
-  ${(props) =>
-    props.$variant === "contained" &&
-    css`
-      background-color: var(--color-primary);
-      color: white;
-    `};
-
-  &:hover {
-    color: var(--color-primary);
-    background-color: var(--color-secondary);
-  }
-`;
+import styled from "styled-components";
+import Button from "@/components/Button/index.js";
 
 const FlexContainer = styled.section`
   display: flex;
   flex-wrap: wrap;
   gap: 4px;
+  flex-direction: ${({ $direction = "row" }) =>
+    $direction === "row" ? "row" : "column"};
 `;
 
 export default function HomePage() {
@@ -56,7 +19,7 @@ export default function HomePage() {
         <Button $color="danger">Button Danger</Button>
       </FlexContainer>
       <h2>Using Props: CSS Block / Complex Component</h2>
-      <FlexContainer>
+      <FlexContainer $direction="column">
         <Button $variant="text">Button - Text</Button>
         <Button $variant="outlined">Button - Outline</Button>
         <Button $variant="contained">Button - Contained</Button>
